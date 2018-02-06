3e5f8953c53576810c90141d5428f532555e6302
Rileybr <31544827+Rileybr@users.noreply.github.com>
2018-02-06 08:46:46 -0500

    first commit

---
+++ Assets/Script/player_controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_controller : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+		
+	}
+	
+	void FixedUpdate ()
+		{	
+			float move_horizontal = Input.GetAxis ("Horizontal");
+			float move_vertical = Input.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (move_horizontal, 0.0f, move_vertical);
+
+		rb.AddForce (movement * speed);
+		}
+
+		void OnTriggerEnter(Collider other) 
+		{
+		if (other.gameObject.CompareTag("Pick Up"))
+			{
+			other.gameObject.SetActive(false);
+			count += 1; 
+			SetCountText ();
+			}
+			if (other.gameObject.CompareTag("Special Pick Up"))
+			{
+				other.gameObject.SetActive(false);
+				count += 3; 
+				SetCountText ();
+			}
+			if (other.gameObject.CompareTag("Game Over Pick Up"))
+			{
+				other.gameObject.SetActive(false);
+				count += 10; 
+				SetCountText ();
+			}
+		}
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 26) {
+			winText.text = "You Win!";
+		}
+	}
+	}
